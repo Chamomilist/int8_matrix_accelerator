258c3fd9e37886555e76d82570fb1c6285df2522
Avyukth <298763275+Chamomilist@users.noreply.github.com>
2026-07-11 02:06:21 +0530

    Multiplier and accumulator has been made

---
+++ rtl/accumulator.sv
@@ -0,0 +1,21 @@
+module accumulator #(parameter int DATA_WIDTH = 32)
+(
+    input logic clk,
+    input logic rst,
+    input logic clear,
+    input logic enable,
+    input logic signed [DATA_WIDTH-1:0] data_in,
+
+    output logic signed [DATA_WIDTH-1:0] acc_out
+);
+
+always_ff @(posedge clk) begin
+    if (rst)
+        acc_out <= '0;
+    else if (clear)
+        acc_out <= '0;
+    else if (enable)
+        acc_out <= acc_out + data_in;
+end
+
+endmodule
---
+++ rtl/multiplier.sv
@@ -0,0 +1,11 @@
+module multiplier #(parameter int DATA_WIDTH = 8)
+(
+    input logic signed [DATA_WIDTH-1:0] a,
+    input logic signed [DATA_WIDTH-1:0] b,
+
+    output logic signed [(2*DATA_WIDTH)-1:0] product
+);
+
+assign product = a * b;
+
+endmodule
